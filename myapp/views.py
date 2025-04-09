836da0c079b5e7a9da75bc5ae1a599cdc790f191
Daniela Esposito <daniela.p.esposito@gmail.com>
2025-04-09 05:52:17 +0100

    avance

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"mensaje": "Bienvenidos a mi aplicación Django"}
+    context = {"mensaje": "Las mejores marcas traídas de todo el mundo"}
     return render(request, "myapp/index.html", context)
